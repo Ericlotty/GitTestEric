24834631f0b988aba4eb27fca1f53f877e243fc1
Smyers46 <123281211+Smyers46@users.noreply.github.com>
2026-07-30 16:29:08 -0700

    Health script

---
+++ Healthtest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Test comment to throw errors
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

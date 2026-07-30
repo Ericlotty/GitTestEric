b561d20a905bc2e6c7529ab91c9f13f113d91413
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2026-07-30 19:38:15 -0400

    change Health script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private int randomNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        randomNumber = 1;
     }
 
     // Update is called once per frame

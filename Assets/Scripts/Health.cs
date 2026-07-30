95dc9d9078df691897b0fc449e3b02229942da3b
Ericlotty <elottsfeldt@gmail.com>
2026-07-30 16:39:21 -0700

    2nd Commit
    fill

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+
+// This is Eric's Health Script
+
+
 public class Health : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
